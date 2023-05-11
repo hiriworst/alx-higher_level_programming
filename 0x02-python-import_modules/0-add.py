8e74d6cbb371dd5ad307ab142b22043a0c01a690
hiriworst <saidhiribae@gmail.com>
2023-05-11 13:21:19 -0700

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+
+# Import statements should come before other code
 from add_0 import add
 
+# Use two blank lines to separate top-level functions and classes
 if __name__ == "__main__":
 
-a = 1
-b = 2
+    # Use four spaces for indentation
+    # Remove unnecessary comment
+    a = 1
+    b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    # Use string formatting to make the code more readable
+    print("{} + {} = {}".format(a, b, add(a, b)))
